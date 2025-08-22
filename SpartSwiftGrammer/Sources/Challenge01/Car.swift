530f1bba0acf8d35e12bafb052a7c0ab822e0d37
Roy <shuwj81@daum.net>
2025-08-22 11:14:09 +0900

    🪛[chore]:  pr 반영

--- SpartSwiftGrammer/Sources/Challenge01/Car.swift
+++ SpartSwiftGrammer/Sources/Challenge01/Car.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 class Car {
-  var brand: String
-  var model: String
-  var year: String
+  private(set) var brand: String
+  private(set) var model: String
+  private(set) var year: String
   var engine: Engine
-  
+
   init(
     brand: String,
     model: String,
@@ -24,15 +24,15 @@ class Car {
     self.year = year
     self.engine = engine
   }
-  
+
   internal func driving() {
     #logDebug("Car 주행 중")
   }
-  
+
   internal func stop() {
     #logDebug("Car 멈추기")
   }
-  
+
   internal func refuel() {
     #logDebug("Car 주유중")
   }
