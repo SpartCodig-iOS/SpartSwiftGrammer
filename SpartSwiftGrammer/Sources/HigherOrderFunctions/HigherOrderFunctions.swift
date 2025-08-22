396b4fa24131693699abc073c4129220f48e0917
Roy <shuwj81@daum.net>
2025-08-22 09:00:07 +0900

    🪛[chore]:  pr 반영

--- SpartSwiftGrammer/Sources/HigherOrderFunctions/HigherOrderFunctions.swift
+++ SpartSwiftGrammer/Sources/HigherOrderFunctions/HigherOrderFunctions.swift
@@ -9,20 +9,20 @@ import Foundation
 import LogMacro
 
 final actor HigherOrderFunctions {
-    var numbers: [Int] =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  var numbers: [Int] =  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-   let numbers1 = [1, 2, 3, 4, 5]
+  let numbers1 = [1, 2, 3, 4, 5]
 
 
 
-   func changeForinToMap() {
+  func mapNumbers() {
     let result = numbers1.map{ String($0) }
-    #logDebug(numbers1)
+    #logDebug(result)
 
   }
 
 
-   func changeType(_  number: [Int]  ) {
+  func changeType(_  number: [Int]  ) {
     let result = number
       .filter { $0 % 2 == 0}
       .map { String($0)}
@@ -30,21 +30,21 @@ final actor HigherOrderFunctions {
     #logDebug("고차 함수 테스트", result)
   }
 
-   func changeTypeWithMyMap(_  number: [Int]  ) {
-    let result = myMap(number) { n in
-           n % 2 == 0 ? String(n) : ""
-       }.filter { !$0.isEmpty }
+  func changeTypeWithMyMap(_ number: [Int]  ) {
+    let result = myMap(number) { number in
+      number % 2 == 0 ? String(number) : ""
+    }.filter { !$0.isEmpty }
     #logDebug("고차 함수 테스트 with MY MAP", result )
   }
 
-   func myMap<T , U>(
+  func myMap<T , U>(
     _ array: [T],
     transform : (T) -> U) -> [U] {
-    var result = [U]()
-    for element in array {
-      result.append(transform(element))
+      var result = [U]()
+      for element in array {
+        result.append(transform(element))
+      }
+      return result
     }
-    return result
-  }
 }
 
